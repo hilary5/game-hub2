ef34a274461ef1d16e88364ac7c1e06693211706
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2025-09-16 00:35:50 +0100

    refactoring -  creating a reusable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,7 +7,7 @@
 //   },
 // });
 
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
@@ -15,9 +15,23 @@ export interface FetchResponse<T> {
 }
 
 
-export default axios.create({
+const axiosInstance =  axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
         key: "fb65bb5513e84f80b6648a226f5b27ce"
     }
 })
+
+class APIClient<T> {
+    endpoint: string;
+  
+    constructor(endpoint: string) {
+        this.endpoint = endpoint
+    }
+
+    getAll = (config: AxiosRequestConfig) => {
+     return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data)
+    }
+}
+
+export default APIClient;
